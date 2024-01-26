87da227ac9447f0f0784c7174e0acec6de9f587d
uchennachibueze <emmachi72.ec@gmail.com>
2024-01-26 17:09:26 +0100

    Update zeronPipelineStageInit.groovy

--- vars/zeronPipelineStageInit.groovy
+++ vars/zeronPipelineStageInit.groovy
@@ -1,5 +1,10 @@
+import org.zeron.pipeline.framework.workflow.*
 import groovy.transform.Field
 import jenkins.model.Jenkins
-// import org.jenkinsci.plugins.workflow
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
 
 @Field String STEP_NAME = getClass().getName()
+
+void call(Map parameters = [:]) {
+  
+}
