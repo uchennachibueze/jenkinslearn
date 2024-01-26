d674431ec9d2e9651152d8d2dd4ae960f08274b6
uchennachibueze <emmachi72.ec@gmail.com>
2024-01-26 18:01:27 +0100

    Create ConfigurationHelper.groovy

---
+++ src/org/zeron/ConfigurationHelper.groovy
@@ -0,0 +1,21 @@
+package org.zeron
+
+@Singleton
+class ConfigurationHelper implements Serializable {
+
+
+  public void loadInitialResources(Script script) {
+    addConfig(script, configFromResource)
+  }
+  
+  private void addConfig(Script script) {
+    Map values = [:]
+    // if (defaultsFromResources != null) {
+    //   values = addDefaultsFromLibraryResources(script)
+    // }
+  }
+  
+  private Map addDefaultsFromLibraryResources(Script script) {
+    Script.echo "Parameters: "
+  }
+}
